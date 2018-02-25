aa3103d727fd25eedd7f88e297d41cb4ca12d103
Peter Ding <hding@umail.ucsb.edu>
2018-02-24 18:09:55 -0800

    update new version

---
+++ app/src/main/java/com/cs48/g15/notehub/User.java
@@ -0,0 +1,27 @@
+package com.cs48.g15.notehub;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Delin Sun on 2/21/2018.
+ */
+
+public class User {
+
+    public String username;
+    public String email;
+    private ArrayList<String> followers;
+
+    public User() {
+        // Default constructor required for calls to DataSnapshot.getValue(User.class)
+    }
+
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    public void add_follower(String username){
+        followers.add(username);
+    }
+}
